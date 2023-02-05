1568eefba331a9bbb723f1233038596d83f1dcbf
0vniSiente <fuentesdavid1505@gmail.com>
2023-02-05 15:14:17 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hola preciosa <code>soy el hombre de tus sueños</code> nunca lo dudes.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click aquí para recuperar tu solteria :C 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
